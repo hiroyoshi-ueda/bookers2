93ffb9bb5771ccdfda4d3cd7e43e3c7be01f6e69
hiroyoshi-ueda <torikara.h1139@gmail.com>
2020-02-18 05:10:06 +0000

    toppage create

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+  def index
+  	  @books = Book.all
+  	  @book = Book.new
+  end
+
+  def show
+  end
+
+  def new
+  	  @book = Book.new
+  end
+
+  def edit
+  end
+
+  def create
+  	  book = Book.new(book_params)
+  	  book.save
+  end
+
+  def destroy
+  	  book = Book.find(params[:id])
+  	  book.destroy
+  	  redirect_to books_path
+  end
+
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :books
+  root "homes#top"
+  devise_for :users
+  resources :users, only: [:show]
+  get 'about' => 'homes#about'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
